f10ecc3bda0a7b5e22183617ec20adbe15736372
Natthasak Vechprasit <codesonteen@gmail.com>
2020-04-04 23:22:51 +0700

    1.0-RELEASE

---
+++ UnattendResources/CustomScripts/RunAfterCloudbaseInitInstall.ps1
@@ -0,0 +1,13 @@
+$sourceConfigDir = 'C:\UnattendResources\Config'
+$targetConfigDir = 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf'
+
+if((Test-Path $sourceConfigDir)) {
+    # Inject Cloudbase-Init Config
+    Copy-Item -Recurse -Force "$sourceConfigDir\cloudbase-init.conf" "$targetConfigDir\cloudbase-init.conf"
+    Copy-Item -Recurse -Force "$sourceConfigDir\cloudbase-init-unattend.conf" "$targetConfigDir\cloudbase-init-unattend.conf"
+}
+
+Write-Host -NoNewLine "[AfterCloudbaseInitInstall] Once you done any manual configuration or don't want to config, Press any key to continue...";
+$key = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+Write-Host "Entering Sysprep phase..."
---
+++ UnattendResources/CustomScripts/RunBeforeSysprep.ps1
@@ -0,0 +1,11 @@
+$sourceLocalScriptPath = 'C:\UnattendResources\LocalScripts'
+$targetLocalScriptPath = 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts'
+
+if(!(Test-Path $targetLocalScriptPath)) {
+    mkdir -Path $taegetLocalScriptPath
+}
+
+if((Test-Path $sourceLocalScriptPath)) {
+    # Inject LocalScripts
+    Copy-Item -Recurse -Force $sourceLocalScriptPath 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\'
+}
